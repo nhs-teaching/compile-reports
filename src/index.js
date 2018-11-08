0630311d37fcdb6573f0a73b3b71f89b2e911234
Josh <joshrasmussen34@gmail.com>
2018-11-07 20:24:15 -0800

    Update to walk through each project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-const { getFiles } = require('./helpers.js');
+const { getFiles, openFile } = require('./helpers.js');
 const { processCheckStyleReport } = require('./check.js');
 const { processTestReports } = require('./testReport.js');
 const path = require('path');
+const Enquirer = require('enquirer');
+const enq = new Enquirer();
+
+enq.register('confirm', require('prompt-confirm'));
+
+enq.question({
+  type: 'confirm',
+  name: 'continue',
+  message: 'Would you like to show the next file?'
+});
+
+async function showSrcFiles(proj) {
+  let srcFiles = await getFiles(proj);
+
+  try {
+     for (srcFile of srcFiles) {
+       let contents = await openFile(srcFile);
+       process.stdout.write(contents);
+     
+       while (!(await enq.ask('continue')).continue) {}
+     }
+  } catch (e) {
+    process.stderr.write(e);
+  }
+}
+
+async function getStudent(proj) {
+  let logLoc = path.resolve(proj, 'output.log');
+
+  let logFile = await openFile(logLoc);
+  
+  console.log(logFile.split('\n').slice(0, 4).join('\n'));
+}
 
 /**
  * Processes the results of the project for tests and checks. 
@@ -9,13 +42,19 @@ const path = require('path');
  * @param proj <string>
  */
 async function processProject(proj) {
-  // get check*.xml
+  await getStudent(proj);
+  while(!(await enq.ask('continue')).continue) {}
+  
+  let srcLoc = path.resolve(proj, 'src', 'main', '**', '*.java');
+  await showSrcFiles(srcLoc); 
+  
   let checkLoc = path.resolve(proj, 'build', 'reports', 'check*.xml');
   await processCheckStyleReport(checkLoc);
 
-  // get tests/*.xml
   let testLoc = path.resolve(proj, 'build', 'reports', 'tests', '*.xml');
   await processTestReports(testLoc);
+
+  while(!(await enq.ask('continue')).continue) {}
 }
 
 (async function main(args) {
